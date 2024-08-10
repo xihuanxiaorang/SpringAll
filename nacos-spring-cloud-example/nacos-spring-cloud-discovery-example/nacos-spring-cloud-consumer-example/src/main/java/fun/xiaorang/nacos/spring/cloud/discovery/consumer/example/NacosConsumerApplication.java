dc1c30919f88672c362af9a5838ea047390638d3
xiaorang <15019474951@163.com>
2024-08-10 10:45:18 +0800

    feat(nacos-discovery): 采用带有负载均衡的RestTemplate的方式进行服务调用

--- nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-spring-cloud-consumer-example/src/main/java/fun/xiaorang/nacos/spring/cloud/discovery/consumer/example/NacosConsumerApplication.java
+++ nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-spring-cloud-consumer-example/src/main/java/fun/xiaorang/nacos/spring/cloud/discovery/consumer/example/NacosConsumerApplication.java
@@ -3,9 +3,8 @@ package fun.xiaorang.nacos.spring.cloud.discovery.consumer.example;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +20,12 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class NacosConsumerApplication {
+  /**
+   * 实例化 RestTemplate 实例
+   * 标注 @LoadBalanced 注解即可在 RestTemplate 上开启 LoadBalanced 负载均衡的功能
+   */
   @Bean
+  @LoadBalanced
   public RestTemplate restTemplate() {
     return new RestTemplate();
   }
@@ -33,21 +37,17 @@ public class NacosConsumerApplication {
   @RestController
   public static class NacosController {
     private final RestTemplate restTemplate;
-    private final LoadBalancerClient loadBalancerClient;
 
     @Value("${spring.application.name}")
     private String appName;
 
-    public NacosController(final RestTemplate restTemplate, final LoadBalancerClient loadBalancerClient) {
+    public NacosController(final RestTemplate restTemplate) {
       this.restTemplate = restTemplate;
-      this.loadBalancerClient = loadBalancerClient;
     }
 
     @GetMapping("/echo/app-name")
     public String echoAppName() {
-      //使用 LoadBalanceClient 和 RestTemplate 结合的方式来访问
-      final ServiceInstance serviceInstance = loadBalancerClient.choose("nacos-provider");
-      final String url = String.format("http://%s:%s/echo/%s", serviceInstance.getHost(), serviceInstance.getPort(), appName);
+      final String url = String.format("http://%s/echo/%s", "nacos-provider", appName);
       System.out.println("request url:" + url);
       return restTemplate.getForObject(url, String.class);
     }
